df7883c49744f7a63a64aa66f0a8b22e511e5991
Dmytro Kharchenko <dmytro190589@gmail.com>
2023-02-10 05:03:12 +0200

    fin

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,17 @@
 import { setFilter } from 'redux/filter';
 import { useDispatch } from 'react-redux';
-import css from './Filter.module.css'
 
 const Filter = () => {
   const dispatch = useDispatch();
   return (
-    <dvi  className={css.findINput}>
+    <>
       <p>Find contacts by name</p>
       <input
        
         type="text"
         onChange={e => dispatch(setFilter(e.currentTarget.value.toLowerCase()))}
       />
-    </dvi>
+    </>
   );
 };
 export default Filter;
